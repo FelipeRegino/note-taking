406a598f18a3891d26560964a3b36064bd70756b
Felipe Regino <liperegino@gmail.com>
2019-05-18 12:20:00 -0300

    Adiciona validação

--- app/view/note.py
+++ app/view/note.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, jsonify
+from flask import Blueprint, request, redirect, jsonify, Response
 from app.database import DB_SESSION
 from ..models import Note
 import datetime
@@ -21,28 +21,39 @@ def get(id):
 @MOD_NOTE.route('/create/note/', methods=['POST'])
 def create():
     r = request.get_json()
-    if r['date']:
+
+    # VALIDAÇÃO
+    if 'date' in r:
         date = datetime.datetime.strptime(r['date'], '%Y-%m-%d')
     else:
         date = None
+
+    if 'title' not in r:
+        return Response('Bad Request: title é obrigatório', status=400)
+    if 'content' not in r:
+        return Response('Bad Request: content é obrigatório', status=400)
+    if 'status' not in r:
+        return Response('Bad Request: status é obrigatório', status=400)
+
     note = Note(r['title'], r['content'], date, r['status'])
     DB_SESSION.add(note)
     DB_SESSION.commit()
-    return jsonify({'status': 200, 'message': 'Nota criada com sucesso!'})
+
+    return Response('Nota criada com sucesso!', status=200)
 
 
 @MOD_NOTE.route('/update/note/<int:id>', methods=['PUT'])
 def update(id):
     r = request.get_json()
-    if r['date']:
+    if 'date' in r:
         date = datetime.datetime.strptime(r['date'], '%Y-%m-%d')
     else:
         date = None
     note = Note.query.filter(Note.id == id).first()
-    note.title = r['title'] if r['title'] else note.title
-    note.content = r['content'] if r['content'] else note.content
+    note.title = r['title'] if 'title' in r else note.title
+    note.content = r['content'] if 'content' in r else note.content
     note.date = date if date else note.date
-    note.status = r['status'] if r['status'] else note.status
+    note.status = r['status'] if 'status' in r else note.status
     DB_SESSION.commit()
     return jsonify({'status': 200, 'message': 'Nota atualizada com sucesso!'})
 
